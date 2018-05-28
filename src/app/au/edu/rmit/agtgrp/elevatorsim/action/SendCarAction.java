aeb9212f1ef06e0ee4e81da73565bb1a4f44df8f
Abhijeet Anand <abhijeet.anand@rmit.edu.au>
2018-05-28 17:20:17 +1000

    Cleanup and Fix change destination check. Fixes issue #35

--- src/app/au/edu/rmit/agtgrp/elevatorsim/action/SendCarAction.java
+++ src/app/au/edu/rmit/agtgrp/elevatorsim/action/SendCarAction.java
@@ -19,7 +19,9 @@ public class SendCarAction extends Action {
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
     private WrapperModel model;
     private int carId;
+    private Car car;
     private int floorId;
+    private Floor floor;
     private Direction nextDirection;
 
     public SendCarAction(long actionId, WrapperModel model, JSONObject params) {
@@ -27,6 +29,9 @@ public class SendCarAction extends Action {
         this.model = model;
         this.carId = params.getInt("car");
         this.floorId = params.getInt("floor");
+
+        this.floor = model.getFloor(floorId);
+        this.car = model.getCar(carId);
 
         String nextDirectionParam = params.getString("nextDirection");
 
@@ -46,60 +51,67 @@ public class SendCarAction extends Action {
 
     @Override
     protected ProcessingStatus performAction() {
-        LOG.debug("Sending car {} to floor {} towards {}", carId, floorId, nextDirection);
+        LOG.debug("Sending car {} to floor {} then {}", carId, floorId, nextDirection);
 
         ProcessingStatus status = ProcessingStatus.FAILED;
-        Floor floor = model.getFloor(floorId);
-        Car car = model.getCar(carId);
 
         if (car == null) {
-            LOG.error("Car with ID {} missing!", carId);
             failureReason = "No car with id: " + carId;
         } else if (floor == null) {
-            LOG.error("Floor with ID {} missing!", floorId);
             failureReason = "No floor with id: " + floorId;
         } else if (nextDirection == Direction.NONE) {
-            LOG.error("nextDirection should not be {}", nextDirection);
-            failureReason = "Invalid value for 'nextDirection' parameter. Valid values are 'up' and 'down'";
-        } else if (LegalChangeDestination()) {
-            LOG.debug("About to set next direction to {}", nextDirection);
+            failureReason = "Next direction cannot be " + nextDirection;
+        } else if (canChangeDestination()) {
+            LOG.debug("About to set next destination to {} in direction {}", floor, nextDirection);
             model.setNextDirection(carId, nextDirection);
             car.setDestination(floor);
             status = ProcessingStatus.IN_PROGRESS;
         } else {
-            LOG.error("Cannot send car, requires illegal change of destination to {} in direction {}", floor, nextDirection);
+            failureReason = "Illegal change in destination. Cannot send to " + floor;
         }
 
         LOG.debug("Processing Status: {}", status);
+        if (status == ProcessingStatus.FAILED) {
+            LOG.error(failureReason);
+        }
 
         return status;
     }
 
-    private boolean LegalChangeDestination() {
-        Floor floor = model.getFloor(floorId);
-        Car car = model.getCar(carId);
-
-        LOG.debug("Checking if legal change of destination to floor {} for car {} with current destination {}",
-                floor, car, car.getDestination());
+    /**
+     * Checks whether the destination can be changed for this action request.
+     * For a given elevator (car), destination can be changed if the request for change is in the direction of current
+     * travel of this car and it can safely stop.
+     *
+     * @return true if destination can be changed, false otherwise
+     */
+    private boolean canChangeDestination() {
+        LOG.debug("Change destination check: {} to {}", car, floor);
         LOG.trace("Call hierarchy for this method:", new Exception());
 
+        // Early exit: If the car wasn't moving, then we can simply send it to where it's been requested
         if (car.getDestination() == null) {
+            LOG.debug("Change destination is allowed");
             return true;
         }
 
         float currentHeight = car.getHeight();
-        float lastDestHeight = car.getDestination().getHeight();
-        float newDestHeight = floor.getHeight();
-        float stoppingDistance = car.getStoppingDistance()
-        Direction currDirection = (currentHeight < lastDestHeight) ? Direction.UP : Direction.DOWN;
+        float currentDestinationHeight = car.getDestination().getHeight();
+        float stoppingDistance = car.getStoppingDistance();
 
-        // To late to change to desired destination, floor requested is passed towards direction...
-        if ((currDirection == Direction.UP && newDestHeight - stoppingDistance < currentHeight ) ||
-        (currDirection == Direction.DOWN && newDestHeight + - stoppingDistance > currentHeight)) {
-            failureReason = "Car " + carId + " already past floor " + floorId;
+        Direction currDirection = (currentHeight < currentDestinationHeight) ? Direction.UP : Direction.DOWN;
+
+        float requestedDestinationHeight = floor.getHeight();
+
+        // Too late to change to desired destination; elevator has gone past the requested floor or not within
+        // safe stopping distance
+        if ((currDirection == Direction.UP && requestedDestinationHeight - currentHeight < stoppingDistance) ||
+                (currDirection == Direction.DOWN && currentHeight - requestedDestinationHeight < stoppingDistance)) {
+            LOG.error("Change destination check failed: {} is past {}", car, floor);
             return false;
         }
 
+        LOG.debug("Change destination is allowed");
         return true;
     }
 
